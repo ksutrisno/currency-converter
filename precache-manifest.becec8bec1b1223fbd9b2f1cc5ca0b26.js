bbddf639888fd00f437f1249ba9fa9b9b3a36e89
ksutrisno <kevinjsutrisno@gmail.com>
2019-10-20 01:08:04 +0700

    Updates

---
+++ precache-manifest.becec8bec1b1223fbd9b2f1cc5ca0b26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3966285559217703bce7e057c3df4537",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "f05e59d4e5225cdc34c1",
+    "url": "/currency-converter/static/css/main.a3bf2a25.chunk.css"
+  },
+  {
+    "revision": "11eadd8629338d8f1485",
+    "url": "/currency-converter/static/js/2.7b8d969a.chunk.js"
+  },
+  {
+    "revision": "f05e59d4e5225cdc34c1",
+    "url": "/currency-converter/static/js/main.7f3af87c.chunk.js"
+  },
+  {
+    "revision": "c0d28daf08c87ac5e666",
+    "url": "/currency-converter/static/js/runtime-main.f8c82292.js"
+  }
+]);
