664ac542afdcd17b9a0b08a8f6efa6982331df00
ksutrisno <kevinjsutrisno@gmail.com>
2019-10-20 02:01:30 +0700

    Updates

---
+++ precache-manifest.5ff9a78e824310d069d6be66ef4176ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5891bb858506b90f71949a3ff63184a8",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "2e633e137217d6db8d1b",
+    "url": "/currency-converter/static/css/main.8414bde3.chunk.css"
+  },
+  {
+    "revision": "103251ce3efa191f6b98",
+    "url": "/currency-converter/static/js/2.e330555f.chunk.js"
+  },
+  {
+    "revision": "2e633e137217d6db8d1b",
+    "url": "/currency-converter/static/js/main.bf4c283d.chunk.js"
+  },
+  {
+    "revision": "c0d28daf08c87ac5e666",
+    "url": "/currency-converter/static/js/runtime-main.f8c82292.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/currency-converter/precache-manifest.2412c87235e932bde491fa3bdceeca07.js"
+  "/currency-converter/precache-manifest.5ff9a78e824310d069d6be66ef4176ff.js"
 );
 
 self.addEventListener('message', (event) => {
